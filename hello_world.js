3bb1c665d6655412fa849774119cd3c74f8ed1e5
Pranav Yadav <138780146+pranavy2915@users.noreply.github.com>
2024-01-04 08:11:25 +0000

    traversing object

--- hello_world.js
+++ hello_world.js
@@ -1,7 +1,24 @@
-console.log("Hello world")
+// console.log("Hello world")
 const arr=[0,1,2,3,4];
 
 for(const num of arr)
 {
-    console.log(num);
+    // console.log(num);
+}
+const n = 'Panny'
+for(const letters of n)
+{
+    // console.log(letters);
+}
+
+
+const myobj ={
+    "name" : "Pranav",
+    "college" : "KIIT",
+    "roll" : 20051156
+}
+
+for(const key in myobj)
+{
+    console.log(`${key} : ${myobj[key]}`);
 }
